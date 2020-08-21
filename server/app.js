c747ff946766092151ab876ec4b86a4e6c6c8b7d
Hugo Cueva <hugo.cueva@owners-link.com>
2020-08-21 18:48:13 -0500

    Added bodyParser

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require('express'); 
+const bodyParser = require('body-parser'); 
+
 const app = express(); 
+
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: false})); 
 
 app.get('/api/user/login', (req,res) => {
     res.send('Hello world!'); 
